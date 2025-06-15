22a4f7fb198254c3f24e99b5ad45b0e20ee6ad8e
lakshayjain <lakshay2007jain@gmail.com>
2025-06-15 20:55:18 +0530

    InternService Linking

---
+++ aiservice/src/main/java/com/fitness/aiservice/CustomException.java
@@ -0,0 +1,7 @@
+package com.fitness.aiservice;
+
+public class CustomException extends RuntimeException {
+    public CustomException(String message) {
+        super(message);
+    }
+}
---
+++ aiservice/src/main/java/com/fitness/aiservice/service/UserValidationService.java
@@ -0,0 +1,27 @@
+package com.fitness.aiservice.service;
+
+import com.fitness.aiservice.CustomException;
+import com.fitness.aiservice.config.WebClientConfig;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+
+@Service
+@AllArgsConstructor
+public class UserValidationService {
+    public final WebClient userServiceWebClient;
+
+    public boolean validateUser(String userId) {
+
+        try {
+            return Boolean.TRUE.equals(userServiceWebClient.get().uri("/api/users/{userId}/validate",userId).retrieve().bodyToMono(boolean.class).block());
+        } catch (WebClientResponseException e) {
+            if (e.getStatusCode() == HttpStatus.NOT_FOUND) throw new CustomException("User Not found:" + userId);
+            if (e.getStatusCode() == HttpStatus.BAD_REQUEST) throw new CustomException("Invalid UserId :" + userId);
+        }
+        return false;
+    }
+}
+
